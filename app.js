7c31f3aa0d95eb082fdac6a8410782e41d61872e
sharmeen466 <sharmeennazeer2@gmail.com>
2025-04-29 05:06:37 +0500

    ToDo List

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const btn = document.getElementById("btn")
 const list = document.getElementById("list")
 const input = document.getElementById("input1")
-console.log(btn);
+
+
 btn.addEventListener("click", function () {
     if (input.value === "") {
         Swal.fire({
@@ -16,9 +17,29 @@ btn.addEventListener("click", function () {
     }
     else {
         const li = document.createElement("li")
+        // const container = document.createElement("div")
+        const deleteBtn = document.createElement("button")
+        deleteBtn.textContent = "X"
+        deleteBtn.classList.add("delete")
+        const edit = document.createElement("button")
+        edit.textContent = "Edit"
+        edit.classList.add("editBtn")
+        const mark = document.createElement("input")
+        mark.type = "checkbox"
+
         li.textContent = input.value
         list.appendChild(li);
+        li.appendChild(mark);
+        li.appendChild(edit)
+        li.appendChild(deleteBtn)
         input.value = ""
+        deleteBtn.addEventListener("click", function () {
+            li.classList.toggle("remove")
+        })
+        mark.addEventListener("click", function () {
+            li.classList.toggle("disable")
+            // li.classList.remove("remove")
+        })
     }
-    // }
 })
+
